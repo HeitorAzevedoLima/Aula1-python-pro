59523594a202c4fd1de123846fef02a43b2f82fd
HeitorAzevedoLima <heitorazevedolima@gmail.com>
2026-08-11 18:04:19 -0300

    Modify command prefix and add new bot functionalities
    Updated bot command prefix and added new commands for rolling dice and sending memes.

--- Bot.py
+++ Bot.py
@@ -1,12 +1,19 @@
+from urllib import request
+
 import discord
 from discord.ext import commands
 import random
+
+import os
+print(os.listdir('Images'))
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='/', description='A simple bot', intents=intents)
+bot = commands.Bot(command_prefix='?', description='bot', intents=intents)
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 @bot.command()
 async def roll(ctx, dice: str):
@@ -19,5 +26,22 @@ async def roll(ctx, dice: str):
 
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
     await ctx.send(result)
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+@bot.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the bot is cool.')
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+@bot.command()
+async def meme(ctx, im: int):
+    with open('Images/mem'+ str(im) + '.png', 'rb') as f:
+        #Vamos armazenar o arquivo convertido da biblioteca do Discord nesta variável!
+        picture = discord.File(f)
+    # Podemos então enviar esse arquivo como um parâmetro
+    await ctx.send(file=picture)
 
 bot.run('???')
